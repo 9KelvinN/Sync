8f0b306a6c9056d51cca05e5378fbe75a2b01157
bzhang0 <bzhang0@cs.washington.edu>
2022-10-22 21:18:23 -0700

    breh

---
+++ src/Splash.js
@@ -0,0 +1,13 @@
+import './styles.css'
+import './splash.css'
+
+function Splash() {
+  return (
+    <div>
+      <h2>23 October 2022</h2>
+      <h1>Hello, Ben!</h1>
+    </div>
+  )
+}
+
+export default Splash;
